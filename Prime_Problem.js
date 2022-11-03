005f31c2dec3a93821f508a4230ea6c668e3b301
Ashutosh6261 <patidarashutosh2748@gmail.com>
2022-11-03 14:21:13 +0530

    this is a prime problem by ashu

---
+++ Prime_Problem.js
@@ -0,0 +1,16 @@
+//print details
+
+let details= {
+  name: "Ashutosh",
+  age: 100,
+  sex: "Male",
+  location: "Ujjain"
+};
+
+// bracket noation
+  console.log(name["location"]);
+  console.log(name["sex"]);
+
+//dot notation
+  console.log(name.name);
+  console.log(name.age);
